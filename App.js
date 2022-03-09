9f6b371cedd3915930f4c9a770b93a10d9927dc2
19043363 <19043363@stu.mmu.ac.uk>
2022-03-09 23:33:30 +0000

    Commented App.js

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
+// Importing react native components
 import "react-native-gesture-handler";
 import React, { Component } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+// Importing screens
 import LogoutScreen from "./screens/logout";
 import BottomTabNavigator from "./navigation/bottomTabNavigator";
 import LoginStackNavigator from "./navigation/loginStackNavigator";
 
+// Create a stack navigator
 const Stack = createStackNavigator();
 
 class App extends Component {
   render() {
     return (
+      // Create a stack navigator with nested navigators
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
